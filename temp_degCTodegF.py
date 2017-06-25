ab7954f9b81ab5371f3bfac65e66275322d98ef1
Suresh <sureshsingam@gmail.com>
2017-06-25 03:16:04 -0300

    Completed file handling exercises

--- temp_degCTodegF.py
+++ temp_degCTodegF.py
@@ -1,8 +1,6 @@
 
 def celciusToFahrenheit(celcius):
-    if float(celcius) < -273.15:
-        return "Cannot process conversion of temperatures lower than -273.15"
-    else:
+    if float(celcius) > -273.15:
         return "{:.2f} Fahrenheit".format((celcius*9/5) + 32)
 '''
 userVal = input("Enter temp in celcius: \t")
@@ -13,6 +11,12 @@ else:
     print("Temperature in celcius in 2 decimal degree {:.2f}: ".format(userValConvert))
 '''
 temperatures=[10,-20,-289,100]
+tempfile = open("tempFile.txt","w")
 
 for temp in temperatures:
     print(" {0} celsius  is {1} ".format(temp,celciusToFahrenheit(temp)))
+    #creating a temp file to write
+    tempfile.write(" {}\n".format(celciusToFahrenheit(temp)))
+
+#loop is over, close temperature file
+tempfile.close()
